fd16a670427b6b23826f06d6d60aa366e85a9d44
PangolinPaw <pangolinpaw@hotmail.co.uk>
2014-11-24 13:37:55 +0000

    Update timelapse.py
    Clarified certain comments & updates website URL

--- timelapse.py
+++ timelapse.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
 
+# -------------------------------------------------------------------------------
 # COTSWOLD JAM TIME-LAPSE DEMO
 # This is a simple python script that automates the raspistill- & MEncoder-based time-lapse process
-# presented at the Nov '14 Cotswold Raspberry Jam.
-# www.pangolinpad.yolasite.com
+# that I brought to the Nov '14 Cotswold Raspberry Jam.
 
-# This is the simplest way I've found of running linux commands in python.
+# Read the full tutorial on: www.pangolinpad.wordpress.com/category/cotswold-jam/november-14/
+# -------------------------------------------------------------------------------
+
+# Using os is the simplest way I've found of running linux commands in python.
 # You'll see it below as os.system('some sort of command'):
 import os		
 
 # The following 4 variables can be set to whatever you like:
 videoName = 'timelapse'	# The name given to the final video
-filename = 'myImage'	# Every timelapse frame's name wil start with this.
+filename = 'myImage'	# Every time-lapse frame's name will start with this.
 interval = 1000		# Time (in milliseconds) between capturing each frame (1 sec = 1,000 millisec)
-duration = 1800000		# Time (in milliseconds) that the timelapse will capture for (1 hr = 360,000 millisec)
+duration = 1800000	# Time (in milliseconds) that the time-lapse will capture for (1 hr = 360,000 millisec)
 
 delete = True		# If this is set to True, the program will delete all the frames once the video is complete
 
 def capture(interval, duration):
-	# This captures the timelapse frames.
+	# This captures the time-lapse frames.
 
 	# You need to pass it two bits of info: the *interval* between shots and the total *duration* you want
-	# the timelapse to cover. These are then included in the raspistill command below:
+	# the timelapse to cover. These are then included in the raspistill command below (filename comes from above):
 	os.system('raspistill -o ' + filename + '_%04d.jpg -tl ' + str(interval) + ' -t ' + str(duration))
 
 	# This will return 'True' to let you know when the entire capture is complete:
 	return True
 
 def list():
-	# Lists all the frames in a single text file for use by MEncoder
+	# Lists all the frames (in order) in a single text file for use by MEncoder
 	os.system('ls %s* > stills.txt' % filename)
 
 def mencoder():
-	# Runs the MEncoder program that merges all of your stills into a video
+	# Runs the MEncoder program that merges all of your stills into a video. I think it work at a pace of roughly 1 frame each second on a Model B.
 	os.system('mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o %s.avi -mf type=jpeg:fps=24 mf://@stills.txt' % videoName)
 	return True
 
